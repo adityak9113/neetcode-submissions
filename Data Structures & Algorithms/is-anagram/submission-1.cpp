7deecdc1840bbed72ad1b7fe5e7c4660e25d21b3
Aditya Kumar <adityak597@yahoo.com>
2026-09-01 18:33:06 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+    {
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        unordered_map<int,int>freqMap;
+
+        for(int i=0;i<s.length();i++)
+        {
+            auto itr=freqMap.find(s[i]);
+
+            if(itr!=freqMap.end())
+            {
+                freqMap[s[i]]++;
+            }
+
+            else
+            {
+                freqMap[s[i]]=1;
+            }
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            auto itr=freqMap.find(t[i]);
+
+            if(itr!=freqMap.end())
+            {
+                freqMap[t[i]]--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        for(auto itr=freqMap.begin();itr!=freqMap.end();itr++)
+        {
+            cout<<"itr->first="<<itr->first<<" itr->second="<<itr->second<<endl;
+            if(freqMap[itr->first]!=0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+
+
+    }
+};
